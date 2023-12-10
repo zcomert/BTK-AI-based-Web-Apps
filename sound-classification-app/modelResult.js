0c2e71d0e0c868629918a7899f7f07a402694175
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-10 11:58:50 +0300

    Sound classification app has been designed.

---
+++ sound-classification-app/modelResult.js
@@ -0,0 +1,9 @@
+const result = [
+    { name: "baris", probability: 0.1 },
+    { name: "ebru", probability: 0.2 },
+    { name: "erkin", probability: 0.1 },
+    { name: "mabel", probability: 0.1 },
+    { name: "melike", probability: 0.1 },
+    { name: "muslum", probability: 0.2 },
+    { name: "sezen", probability: 0.2 }
+]
