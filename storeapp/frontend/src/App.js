0eadfefa1759fa8b4c0791541281707fe7df4150
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-17 10:19:26 +0300

    Components have been defined.

--- storeapp/frontend/src/App.js
+++ storeapp/frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './basic/Counter';
 import EmployeeList from './basic/EmployeeList';
+import { Link, Route, Routes } from 'react-router-dom';
+import Contact from './basic/Contact';
 
 
 function App() {
@@ -10,8 +12,25 @@ function App() {
       <div>
         Merhaba React.
       </div>
-      <Counter />
-      <EmployeeList />
+
+      <Routes>
+        <Route path='/counter' element={<Counter />}></Route>
+        <Route path='/employees' element={<EmployeeList />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+      </Routes>
+
+      <ul>
+        <li>
+          <Link to="/employees">Çalışanlar</Link>
+        </li>
+        <li>
+          <Link to="/counter">Sayaç</Link>
+        </li>
+        <li>
+          <Link to="/contact">İletişim</Link>
+        </li>
+      </ul>
+
     </div>
   );
 }
---
+++ storeapp/frontend/src/basic/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <ul>
+                <li>Telefon: 506 111 22 33</li>
+                <li>Adres BTK Akademi Samsun</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Contact;
