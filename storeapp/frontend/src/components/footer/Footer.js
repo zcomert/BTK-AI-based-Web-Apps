1e5f62c07ce4ff32cfe2de685dbac79409ab265e
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-17 10:39:56 +0300

    Components have been defined.

---
+++ storeapp/frontend/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <p>
+                Tüm hakları saklıdır.
+            </p>
+            <p>&copy; 2023</p>
+        </div>
+    )
+}
---
+++ storeapp/frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+      <div>
+          <ul>
+              <li>
+                  <Link to="/employees">Çalışanlar</Link>
+              </li>
+              <li>
+                  <Link to="/counter">Sayaç</Link>
+              </li>
+              <li>
+                  <Link to="/contact">İletişim</Link>
+              </li>
+          </ul>
+    </div>
+  )
+}
