551a7b22fcc4edf372c6169f7ca566d789d7fc6d
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-23 12:00:11 +0300

    Book form has been completed.

--- storeapp/frontend/src/basic/Book.js
+++ storeapp/frontend/src/basic/Book.js
@@ -1,35 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Book() {
 
-    let values = {
+    const initialValue = {
         title: '',
         price: '',
         author: ''
-    }
+    };
+
+    const [values, setValues] = useState(initialValue);
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        values = {
-            title: e.target.title.value,
-            price: e.target.price.value,
-            author: e.target.author.value
-        }
         console.log(values);
+        setValues(initialValue);
+        // e.target.reset()
     }
 
     const handleChange = (e) => {
-
-
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
     }
 
     return (
         <div>
             <h1>Kitap Formu</h1>
             <form onSubmit={handleSubmit} >
-                <input onChange={handleChange} type="text" name="title" placeholder='Kitap adı' />
-                <input onChange={handleChange} type="text" name="price" placeholder='Fiyatı' />
-                <input onChange={handleChange} type="text" name="author" placeholder='Yazarı' />
+                <input onChange={handleChange}
+                    type="text" name="title"
+                    placeholder='Kitap adı'
+                    value={values?.title} />
+
+
+                <input onChange={handleChange}
+                    value={values?.price}
+                    type="text"
+                    name="price"
+                    placeholder='Fiyatı' />
+                <input
+                    value={values?.author}
+                    onChange={handleChange}
+                    type="text"
+                    name="author"
+                    placeholder='Yazarı' />
                 <input type="submit" />
             </form>
             {JSON.stringify(values)}
