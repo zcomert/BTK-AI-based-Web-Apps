23e1f744c0e275b78b3d5e9bb14a1ea7abeb8825
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-23 14:23:34 +0300

    Formik has been used.

---
+++ storeapp/frontend/src/basic/BookValidation.js
@@ -0,0 +1,16 @@
+import * as Yup from 'yup';
+
+const BookValidationSchema = Yup.object().shape({
+    title: Yup.string().required('Kitap adı boş bırakılamaz'),
+
+    price: Yup
+        .number()
+        .positive('Sifirdan kucuk olamaz')
+        .required('Fiyat bilgisi boş bırakılamaz')
+        .min(10, 'Fiyatı 10 küçük olamaz.')
+        .max(1000, 'Fiyat 1000 den büyük olamaz'),
+
+    author: Yup.string().required('Yazar bilgisi boş bırakılamaz'),
+});
+
+export default BookValidationSchema;
