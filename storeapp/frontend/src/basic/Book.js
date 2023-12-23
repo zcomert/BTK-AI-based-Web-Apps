eccea9e12f6c4fafa62f90b0a0c0e35733911845
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-23 11:26:37 +0300

    Book component has been defined.

--- storeapp/frontend/src/basic/Book.js
+++ storeapp/frontend/src/basic/Book.js
@@ -2,25 +2,37 @@ import React from 'react'
 
 function Book() {
 
-    
+    let values = {
+        title: '',
+        price: '',
+        author: ''
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(e.target);
+        values = {
+            title: e.target.title.value,
+            price: e.target.price.value,
+            author: e.target.author.value
+        }
+        console.log(values);
     }
-    
+
     const handleChange = (e) => {
-        console.log(e.target.value)
+
+
     }
-    
+
     return (
         <div>
             <h1>Kitap Formu</h1>
             <form onSubmit={handleSubmit} >
-                <input onChange={handleChange} type="text" name="title" placeholder='Kitap adı'  />
-                <input type="text" name="price" placeholder='Fiyatı' />
-                <input type="text" name="author" placeholder='Yazarı' />
+                <input onChange={handleChange} type="text" name="title" placeholder='Kitap adı' />
+                <input onChange={handleChange} type="text" name="price" placeholder='Fiyatı' />
+                <input onChange={handleChange} type="text" name="author" placeholder='Yazarı' />
                 <input type="submit" />
             </form>
+            {JSON.stringify(values)}
         </div>
     )
 }
