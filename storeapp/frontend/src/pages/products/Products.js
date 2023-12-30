30cf7bb45b21e325d9fb636da0a072c2cbf7c0cd
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-30 14:18:53 +0300

    AppContext in use.

--- storeapp/frontend/src/pages/products/Products.js
+++ storeapp/frontend/src/pages/products/Products.js
@@ -4,6 +4,8 @@ import Header from '../../components/header/Header'
 // import ProductService from '../../services/ProductService'
 // import { products } from './ProductsData'
 import AppContext from '../../context/AppContext'
+import { Container, Grid } from '@mui/material';
+import ProductCard from '../product/productCart/ProductCart';
 export default function Products() {
 
     const { products } = useContext(AppContext);
@@ -11,12 +13,16 @@ export default function Products() {
     return (
         <div>
             <Header data={{ title: "Ürünler" }}></Header>
-            <ul>
-                {products?.map((prd) => (<li key={prd?.id} >
-                    <Link to={`/productdetail/${prd?.id}`}>{prd?.name}</Link>
-                </li>))}
-            </ul>
-
+            <Container>
+                <Grid container spacing={3}>
+                    {products?.map((prd) => (
+                        <Grid key={prd.id} item xs={4}>
+                            {/* <Link to={`/productdetail/${prd?.id}`}>{prd?.name}</Link> */}
+                            <ProductCard />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
         </div>
     )
 }
