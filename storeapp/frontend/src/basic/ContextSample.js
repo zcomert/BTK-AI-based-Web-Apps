b4b22841838ae19f6f4fce231714dc13d13b0f55
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-30 12:13:59 +0300

    AppContext in use.

--- storeapp/frontend/src/basic/ContextSample.js
+++ storeapp/frontend/src/basic/ContextSample.js
@@ -1,13 +1,27 @@
 import React, { useContext } from 'react'
 import AppContext from '../context/AppContext'
+import { Button, Container, Stack, TextField } from '@mui/material';
 
 export default function ContextSample() {
-    const message = useContext(AppContext);
+    const { message, setMessage } = useContext(AppContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.text.value);
+        setMessage(e.target.text.value);
+    }
+
     return (
-        <div>
-            {message}
-            <input name="text" ></input>
-            <button type="submit">Gönder</button>
-        </div>
+        <Container>
+            <Stack spacing={3}>
+                <div>
+                    {message}
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <TextField name="text" ></TextField>
+                    <Button type="submit">Gönder</Button>
+                </form>
+            </Stack>
+        </Container>
     )
 }
--- storeapp/frontend/src/basic/Counter.js
+++ storeapp/frontend/src/basic/Counter.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState } from 'react';
 import Header from '../components/header/Header';
+import AppContext from '../context/AppContext';
+import { Link } from 'react-router-dom';
 
 function Counter() {
-
+    
     const [number, setNumber] = useState(0);
-
+    
     function increase() {
         setNumber(number + 1)
         console.log(number);
     }
-
+    
     function decrease() {
         setNumber(number - 1);
         console.log(number);
     }
-
+    
+    const { message } = useContext(AppContext);
     return (
         <div>
             <Header data={{ title: "Sayaç" }}></Header>
+            <h1>{message}</h1>
             {number}
             <button onClick={increase} >Artır</button>
             <button onClick={decrease} >Azaltmak</button>
+            <div>
+                <Link to="/context">Context</Link>
+            </div>
         </div>
     )
 }
