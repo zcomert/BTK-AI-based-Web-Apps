3d0532800989bc5f5100edb3fb0b3921e160adf1
Zafer CÖMERT <zafer.comert@hotmail.com>
2023-12-30 13:49:50 +0300

    AppContext in use.

--- storeapp/frontend/src/context/AppContext.js
+++ storeapp/frontend/src/context/AppContext.js
@@ -1,10 +1,24 @@
-const { createContext, useState } = require("react");
+import {
+    createContext,
+    useState,
+    useEffect
+} from "react";
+
+import ProductService from "../services/ProductService";
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
 
     const [message, setMessage] = useState("Selam");
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        const productService = new ProductService();
+        productService
+            .getAllProducts()
+            .then(resp => setProducts(resp));
+    }, []);
 
     const values = {
         message,
