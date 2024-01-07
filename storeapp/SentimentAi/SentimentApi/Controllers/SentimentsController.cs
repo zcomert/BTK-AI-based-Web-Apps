c918f6cb2a16c444a34109fefe1e4ac8cb3eb3b6
Zafer CÖMERT <zafer.comert@hotmail.com>
2024-01-07 16:18:04 +0300

    ML in use.

---
+++ storeapp/SentimentAi/SentimentApi/Controllers/SentimentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SentimentConsole;
+using static SentimentConsole.SentimentModel;
+
+namespace SentimentApi.Controllers;
+
+[ApiController]
+[Route("/api/predict")]
+public class SentimentsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult MakePrediction([FromBody] ModelInput input)
+    {
+        // Make a single prediction on the sample data and print results
+        var predictionResult = SentimentModel.Predict(input);
+        
+        return Ok(predictionResult);
+    }
+}
